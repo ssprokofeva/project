f000b8c73dc5751e1e56aff014485cd6da12e68d
Софья Прокофьева <fine.prokofjeva@yandex.ru>
2025-04-08 22:30:18 +0300

    /

---
+++ project/Classes/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class DataAccess
+    {
+        public void AddCategory(string name)
+        {
+            using (var context = new ProjectContext())
+            {
+                var category = new Category { Name = name };
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ project/Classes/ProjectContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext() : base("name=ProjectContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Exhibition> Exhibitions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Exhibition>()
+                .HasRequired(e => e.Category)
+                .WithMany()
+                .HasForeignKey(e => e.CategoryId);
+
+            modelBuilder.Entity<Participant>()
+                .HasRequired(p => p.Exhibition)
+                .WithMany(e => e.Participants)
+                .HasForeignKey(p => p.ExhibitionId);
+        }
+    }
+}
