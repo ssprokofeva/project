f319d5d95c369649624f9d826fc630491d45924e
Софья Прокофьева <fine.prokofjeva@yandex.ru>
2025-04-08 21:28:01 +0300

    пару классов+ бд

---
+++ project/Exhibition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class Exhibition
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime JubileeDate { get; set; }
+        public int CategoryId { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Participant> Participants { get; set; }
+    }
+}
+
---
+++ project/Participant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public int ExhibitionId { get; set; }
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+
+        public virtual Exhibition Exhibition { get; set; }
+    }
+}
